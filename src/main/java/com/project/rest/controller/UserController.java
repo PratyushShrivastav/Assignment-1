055d90391d5a0fd99bd8c72f03fa0806bdf418ef
praty <praty@LAPTOP-M11050IF>
2022-01-13 01:13:15 +0530

    testing request parameters value in the api

--- src/main/java/com/project/rest/controller/UserController.java
+++ src/main/java/com/project/rest/controller/UserController.java
@@ -2,10 +2,18 @@ package com.project.rest.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+    @GetMapping("/user")
+    public String getUserDetails(@RequestParam int userId){
+
+        System.out.println(userId);
+        return "hey";
+    }
+
     @GetMapping("/testapiUser")
     public String helloUser(){
         return "hey";
